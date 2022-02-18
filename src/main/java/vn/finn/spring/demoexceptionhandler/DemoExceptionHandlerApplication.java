24f5a496f164ab2487921f03588437cb3076f6f7
finn-01 <nguyensam576@gmail.com>
2022-02-18 14:20:33 +0700

    set-up

---
+++ src/main/java/vn/finn/spring/demoexceptionhandler/DemoExceptionHandlerApplication.java
@@ -0,0 +1,13 @@
+package vn.finn.spring.demoexceptionhandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExceptionHandlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExceptionHandlerApplication.class, args);
+    }
+
+}
